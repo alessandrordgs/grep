c7adb897b5f9b9e13abc2d7cdb5c9ac5c62bb5d7
alessandrordgs <ale.silva1120@gmail.com>
2022-07-10 23:19:37 -0300

    feat: funcionalidade search add e tests

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,17 @@
 use std::env;
-use std::fs;
 use std::process;
+use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parssing arguments: {}", err);
         process::exit(1);
     });
-    println!("In file {}", config.filename);
+    
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
 
-    let contents = fs::read_to_string(config.query).expect("Somethig went wrong reading the file");
-
-    println!("With text:\n{}", contents)
-}
-struct Config {
-    query: String,
-    filename: String,
-}
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+        process::exit(1);
     }
 }
+
